00abcc40c1db76f4cca05719f07ad95faef2bd04
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2019-09-02 17:59:37 +0200

    improvement(units): Add JSDocs

--- packages/units/source/index.ts
+++ packages/units/source/index.ts
@@ -1,15 +1,56 @@
-const KIBI = 1024
+// tslint:disable: no-magic-numbers
 
 /**
- * Binary Prefixes (1024-based)
+ * Binary Prefixes (IEC, 1024-based)
+ *
+ * @see {@link https://en.wikipedia.org/wiki/Binary_prefix}
  */
 export enum BinaryPrefixes {
-	'Ki' = KIBI,
-	'Mi' = KIBI * KIBI,
-	'Gi' = KIBI * KIBI * KIBI,
-	'Ti' = KIBI * KIBI * KIBI * KIBI,
-	'Pi' = KIBI * KIBI * KIBI * KIBI * KIBI,
-	'Ei' = KIBI * KIBI * KIBI * KIBI * KIBI * KIBI,
-	'Zi' = KIBI * KIBI * KIBI * KIBI * KIBI * KIBI * KIBI,
-	'Yi' = KIBI * KIBI * KIBI * KIBI * KIBI * KIBI * KIBI * KIBI,
+	/**
+	 * Kibi
+	 * 1024
+	 */
+	'Ki' = Math.pow(1024, 1),
+
+	/**
+	 * Mibi
+	 * 1048576
+	 */
+	'Mi' = Math.pow(1024, 2),
+
+	/**
+	 * Gibi
+	 * 1073741824
+	 */
+	'Gi' = Math.pow(1024, 3),
+
+	/**
+	 * Tebi
+	 * 1099511627776
+	 */
+	'Ti' = Math.pow(1024, 4),
+
+	/**
+	 * Pebi
+	 * 1125899906842624
+	 */
+	'Pi' = Math.pow(1024, 5),
+
+	/**
+	 * Exbi
+	 * 1152921504606846976
+	 */
+	'Ei' = Math.pow(1024, 6),
+
+	/**
+	 * Zebi
+	 * 1180591620717411303424
+	 */
+	'Zi' = Math.pow(1024, 7),
+
+	/**
+	 * Yobi
+	 * 1208925819614629174706176
+	 */
+	'Yi' = Math.pow(1024, 8),
 }
